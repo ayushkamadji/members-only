ad0f6b13585df05ba5798d5f25fe7e621573b4de
Ayushka Partohap <partohap.ayushka@gmail.com>
2017-05-08 13:18:19 +0700

    Add user sign up

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in(@user)
+      remember(@user)
+      flash[:success] = "Welcome to the members only club"
+      redirect_to root_path
+    else
+      flash[:error] = "Please input valid information"
+      render :new
+    end
+  end
+
+
+  private 
+
+
+    def user_params
+      params.require(:user).permit(:name, 
+                                   :email, 
+                                   :password, 
+                                   :password_confirmation)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/sign_in', to: 'sessions#new'
   post '/sign_in', to: 'sessions#create'
   delete '/sign_out', to: 'sessions#destroy'
+  get '/sign_up', to: 'users#new'
 
+  resources :users, only: [:create]
   resources :posts, only: [:index, :create]
 end
